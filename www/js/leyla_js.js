6c83abed997eb5c09f269d6bf001077652c72651
Lars Bergmann <lb@perfact.de>
2019-11-16 18:40:04 +0100

    Add p5js

---
+++ www/js/leyla_js.js
@@ -0,0 +1,18 @@
+function setup() {
+  createCanvas(640, 480);
+
+  loadImage('img/background.png', img => {
+    image(img, 0, 0);
+  });
+}
+
+function draw() {
+    /*
+  if (mouseIsPressed) {
+    fill(0);
+  } else {
+    fill(255);
+  }
+  ellipse(mouseX, mouseY, 80, 80);
+  */
+}
