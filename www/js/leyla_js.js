e1bc899aaff76f4bc67a4afd533b6adb24ef895d
Lars Bergmann <lb@perfact.de>
2019-11-16 21:57:20 +0100

    Add p5play

--- www/js/leyla_js.js
+++ www/js/leyla_js.js
@@ -1,18 +1,43 @@
+var background_img;
+
+var gamechar_sp;
+var gamechar_img;
+
+var lamp_sp;
+var lamp_img;
+
 function setup() {
-  createCanvas(640, 480);
+    createCanvas(640, 480);
 
-  loadImage('img/background.png', img => {
-    image(img, 0, 0);
-  });
+    background_img = loadImage('img/background_full.png');
+    background_img.resize(640, 480);
+
+    gamechar_sp = createSprite(50,440);
+    gamechar_img = loadImage('img/char_small.png');
+    gamechar_sp.addImage(gamechar_img);
+    //gamechar_sp.addAnimation('normal', 'img/char.png');
+
 }
-
 function draw() {
-    /*
-  if (mouseIsPressed) {
-    fill(0);
-  } else {
-    fill(255);
-  }
-  ellipse(mouseX, mouseY, 80, 80);
-  */
+    background(background_img);
+
+    //gamechar_sp.velocity.x = 0;
+    //gamechar_sp.velocity.y = 0;
+
+    if (keyWentDown('a') || mouseWentDown(RIGHT)) {
+        gamechar_sp.velocity.x = -5;
+    }
+    if (keyWentUp('a') || mouseWentUp(RIGHT)) {
+        gamechar_sp.velocity.x = 0;
+    }
+
+    if (keyWentDown('d') || mouseWentDown(LEFT)) {
+        gamechar_sp.velocity.x = 5;
+    }
+    if (keyWentUp('d') || mouseWentUp(LEFT)) {
+        gamechar_sp.velocity.x = 0;
+    }
+
+
+    drawSprites();
 }
