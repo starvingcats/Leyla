8840af69bac266bbbc7f22b49f000bb3073d69bf
Lars Bergmann <lb@perfact.de>
2019-12-08 20:09:56 +0100

    Telescope lookthrough and text in lvl 1

--- www/js/chapter/leyla_1.js
+++ www/js/chapter/leyla_1.js
@@ -6,6 +6,10 @@ var gamechar_img;
 var telescope_sp;
 var telescope_img;
 var telescope_off_check = false;
+var telescope_seen = false;
+
+var telescope_stars_img;
+
 
 var ground_sp;
 var platforms = [];
@@ -22,6 +26,8 @@ function setup() {
     //background_img.resize(SCENE_W / 2, SCENE_H / 2);
     background_img.resize(SCENE_W, SCENE_H);
 
+    telescope_stars_img = loadImage('img/stars_splash.png');
+
     telescope_sp = createSprite(450, 400);
     telescope_img = loadImage('img/telescope_small.png');
     telescope_sp.addImage(telescope_img);
@@ -31,22 +37,41 @@ function setup() {
 
 }
 function draw() {
-  background(background_img);
-
-    apply_gravity();
-    basic_movement();
-
-    if (keyWentDown('f')) {
-        gamechar_sp.overlap(telescope_sp, function() {
-            telescope_off_check = true;
-        });
+    if (telescope_off_check) {
+        background(telescope_stars_img);
+    } else {
+        background(background_img);
     }
 
-    if ((telescope_off_check) && (gamechar_sp.position.x > 620)) {
-        document.location.href = 'chapter2.html';
-    }
+  if (keyWentDown('f')) {
+      if (telescope_off_check) {
+          telescope_off_check = false;
+          return;
+      }
+      gamechar_sp.overlap(telescope_sp, function() {
+          telescope_off_check = true;
+          telescope_seen = true;
+      });
+  }
 
-    check_scene_bounds();
-    drawSprites();
+  if (telescope_off_check) {
+      return;
+  }
+
+
+  apply_gravity();
+  basic_movement();
+
+  if ((telescope_seen) && (gamechar_sp.position.x > 620)) {
+      document.location.href = 'chapter2.html';
+  }
+
+  check_scene_bounds();
+  drawSprites();
+
+  if (telescope_seen) {
+      fill("white");
+      text('Jetzt muss ich nach Hause gehen...', 320, 240);
+  }
 
 }
