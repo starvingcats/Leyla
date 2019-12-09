e2ddd0656f70b95df493474002b878dcc00a275e
Lars Bergmann <lb@perfact.de>
2019-12-09 15:53:33 +0100

    Dialogues from json file wip

--- www/js/chapter/leyla_1.js
+++ www/js/chapter/leyla_1.js
@@ -17,7 +17,24 @@ var platforms = [];
 var SCENE_W = 1280;
 var SCENE_H = 960;
 
+var dialogues_raw = {};
+var dialogues = {};
+var cur_dialogue = 'intro';
+var cur_dialogue_step = 0;
+
+
+function preload() {
+    dialogues_raw = loadJSON("json/dialogues.json");
+}
+
 function setup() {
+
+    dialogues = dialogues_raw.chapter1;
+    console.log(dialogues_raw);
+    console.log(dialogues);
+
+    //console.log(dialogues);
+
     createCanvas(SCENE_W / 2, SCENE_H / 2);
 
     create_gamechar();
@@ -43,35 +60,40 @@ function draw() {
         background(background_img);
     }
 
-  if (keyWentDown('f')) {
-      if (telescope_off_check) {
-          telescope_off_check = false;
-          return;
-      }
-      gamechar_sp.overlap(telescope_sp, function() {
-          telescope_off_check = true;
-          telescope_seen = true;
-      });
-  }
+    if (keyWentDown('f')) {
+        if (telescope_off_check) {
+            telescope_off_check = false;
+            return;
+        }
+        if (gamechar_sp.overlap(telescope_sp)) {
+            telescope_off_check = true;
+            telescope_seen = true;
+            cur_dialogue = 'outro';
+            cur_dialogue_step = 0;
+        } else {
+            cur_dialogue_step += 1;
+        }
+    }
 
-  if (telescope_off_check) {
-      return;
-  }
+    if (telescope_off_check) {
+        return;
+    }
 
+    apply_gravity();
+    basic_movement();
 
-  apply_gravity();
-  basic_movement();
+    if ((telescope_seen) && (gamechar_sp.position.x > 620)) {
+        document.location.href = 'chapter2.html';
+    }
 
-  if ((telescope_seen) && (gamechar_sp.position.x > 620)) {
-      document.location.href = 'chapter2.html';
-  }
+    check_scene_bounds();
+    drawSprites();
 
-  check_scene_bounds();
-  drawSprites();
-
-  if (telescope_seen) {
-      fill("white");
-      text('Jetzt muss ich nach Hause gehen...', 320, 240);
-  }
-
+    var cur_dialogue_list = dialogues[cur_dialogue];
+    var cur_text = cur_dialogue_list[cur_dialogue_step];
+    if (cur_text) {
+        fill("white");
+        var display_text = cur_text.role + ': ' + cur_text.text;
+        text(display_text, 320, 240);
+    }
 }
