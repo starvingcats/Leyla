06b574178b83054bb093e78b1b5ffea062fd068e
Lars Bergmann <lb@perfact.de>
2020-01-26 15:50:56 +0100

    wip new assets used in lvl3

--- www/js/chapter/leyla_3.js
+++ www/js/chapter/leyla_3.js
@@ -1,9 +1,9 @@
 var background_img;
+var background_sp;
 
 var house_sp;
 var house_on_img;
 var house_off_img;
-
 var house_off_check = false;
 
 var telescope_sp;
@@ -11,31 +11,40 @@ var telescope_img;
 var telescope_off_check = false;
 var telescope_seen = false;
 
+var car_img;
+var car_sp;
+
 var telescope_stars_img;
 
 function setup() {
 
+    has_camera = true;
+
     dialogues = dialogues_raw.chapter3;
 
     createCanvas(SCENE_W, SCENE_H);
+    SCENE_RBOUND = 1000;
 
-    background_img = loadImage('img/bg_city.png');
+    background_img = loadImage('img/lvl3_background.png');
+    background_sp = createSprite(0, 366);
+    background_sp.addImage(background_img);
 
-    telescope_stars_img = loadImage('img/citysky_splash.png');
+    telescope_stars_img = loadImage('img/lvl3_telescopeview.png');
 
-    create_gamechar();
     create_ground();
     create_textbox();
 
-    house_sp = createSprite(3 * SCENE_W / 4, SCENE_GROUND - 130);
-    house_on_img = loadImage('img/house_small.png');
-    house_off_img = loadImage('img/house_small_dark.png');
+    house_sp = createSprite(SCENE_W / 5, SCENE_GROUND - 300);
+    house_on_img = loadImage('img/lvl3_house.png');
+    house_off_img = loadImage('img/lvl3_house_off.png');
     house_sp.addImage('on', house_on_img);
     house_sp.addImage('off', house_off_img);
 
     telescope_sp = createSprite(SCENE_W / 2, SCENE_GROUND - 60);
-    telescope_img = loadImage('img/telescope_small.png');
+    telescope_img = loadImage('img/telescope.png');
     telescope_sp.addImage(telescope_img);
+
+    create_gamechar();
 
 }
 
@@ -43,12 +52,13 @@ function draw() {
 
     if ((house_off_check) && (telescope_off_check)) {
         background(telescope_stars_img);
-    } else {
+    }/* else {
         background(background_img);
-    }
+    }*/
 
   apply_gravity();
   basic_movement();
+  focus_gamechar();
   if (keyWentDown('f')) {
       if (telescope_off_check) {
           telescope_off_check = false;
@@ -87,5 +97,5 @@ function draw() {
   check_scene_bounds();
   drawSprites();
   run_dialogue();
-
+  camera.off();
 }
