4e984bafabf3d0f073f024839286616036fede80
Lars Bergmann <lb@erfact.de>
2020-06-19 18:56:49 +0200

    Better build button in p5js to avoid layout hazzle and stay consistent

--- www/js/chapter/leyla_start.js
+++ www/js/chapter/leyla_start.js
@@ -7,6 +7,13 @@ function setup() {
     background_img = loadImage('img/startscreen.png');
     background_img.resize(SCENE_W, SCENE_H);
 
+    button = createButton('&#128362;');
+    button.position(SCENE_W / 2, SCENE_H - 100);
+    button.mousePressed(dummy);
+}
+
+function dummy() {
+    console && console.log("User Interaction done, audio should work now");
 }
 
 function draw() {
